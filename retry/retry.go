67c2e78569fdfbe5e94bf75ced5cb7c4cad17df7
Shaoqi Bai <bshaoqi@vmware.com>
2022-10-17 17:22:23 -0700

    Change lib name to greenplum-release-engineering-libraries
    Authored-by: Shaoqi Bai <bshaoqi@vmware.com>

--- retry/retry.go
+++ retry/retry.go
@@ -4,7 +4,7 @@ import (
 	"errors"
 	"time"
 
-	"github.com/greenplum-db/gp-releng-libs/vlogs"
+	"github.com/greenplum-db/greenplum-release-engineering-libraries/vlogs"
 
 	"github.com/avast/retry-go"
 )
